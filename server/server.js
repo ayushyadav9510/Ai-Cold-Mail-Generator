889ccb7dd7938b567d8fe937eb846ec3a5254aef
Ayush Yadav <aadiy6419@gmail.com>
2026-09-16 13:43:26 +0530

    Bypassed path-to-regexp parser engine for single link deployment

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '../client/ai-cold-mail-generator/dist')));
 
-app.get('(.*)', (req, res) => {
+ app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/ai-cold-mail-generator/dist', 'index.html'));
 });
 
